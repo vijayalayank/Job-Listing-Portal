d2cd441d98a80d037f4924add12f881c9a91549d
kolluru0305 <KVIDYA.SNC@GMAIL.COM>
2025-10-21 17:00:59 +0530

    App.jsx

--- JobPortal-FrontEnd/src/App.jsx
+++ JobPortal-FrontEnd/src/App.jsx
@@ -1,20 +1,28 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import './App.css'
-import { Register } from './components/Auth/Auth';
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Register/Home";
+import About from "./pages/Register/About";
+import Register from "./pages/Register/Register";
+import "./App.css";
 
 function App() {
-  
-
   return (
-    <>
-      <BrowserRouter>
-          <Routes>
-            {/* <Route path='/' element={<Home/>}/> */}
-            <Route path='/register' element={<Register/>}/>
-          </Routes>
-      </BrowserRouter>
-    </>
-  )
+    <BrowserRouter>
+      <nav className="navbar">
+        <div className="logo">JobPortal</div>
+        <div className="nav-links">
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/register">Register/Login</Link>
+        </div>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
